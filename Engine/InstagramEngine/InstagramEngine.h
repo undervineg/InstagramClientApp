4a3d9177eca96b5d07746a4113778cd892d2dcef
undervine <undervine@gmail.com>
2019-01-01 00:19:38 +0900

    Add project files

---
+++ Engine/InstagramEngine/InstagramEngine.h
@@ -0,0 +1,19 @@
+//
+//  InstagramEngine.h
+//  InstagramEngine
+//
+//  Created by 심승민 on 01/01/2019.
+//  Copyright © 2019 심승민. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for InstagramEngine.
+FOUNDATION_EXPORT double InstagramEngineVersionNumber;
+
+//! Project version string for InstagramEngine.
+FOUNDATION_EXPORT const unsigned char InstagramEngineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InstagramEngine/PublicHeader.h>
+
+
