a845f8e66637f2a919377a0db30bbc006fcd295b
undervine <undervine@gmail.com>
2019-01-03 02:09:34 +0900

    Reorganize folders

--- App/InstagramClientApp/AppDelegate.swift
+++ App/InstagramClientApp/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+//import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,9 +17,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.makeKeyAndVisible()
-        window?.rootViewController = RegisterUserViewController()
+//        FirebaseApp.configure()
+//        
+//        window = UIWindow(frame: UIScreen.main.bounds)
+//        window?.makeKeyAndVisible()
+//        window?.rootViewController = RegisterUserViewController()
         
         return true
     }
