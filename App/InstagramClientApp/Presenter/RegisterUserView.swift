836902840491f919937aee1773776f4d5d732f70
undervine <undervine@gmail.com>
2019-01-09 04:39:10 +0900

    Reorganize directories

---
+++ App/InstagramClientApp/Presenter/RegisterUserView.swift
@@ -0,0 +1,13 @@
+//
+//  RegisterUserView.swift
+//  InstagramClientApp
+//
+//  Created by 심승민 on 09/01/2019.
+//  Copyright © 2019 심승민. All rights reserved.
+//
+
+import Foundation
+
+protocol RegisterUserView {
+    func display(_ errorMessage: String)
+}
