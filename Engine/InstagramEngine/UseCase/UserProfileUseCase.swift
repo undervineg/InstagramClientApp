4e1f3f717445c527c78b6203dffeda6b4fdace81
undervine <undervine@gmail.com>
2019-01-09 04:43:17 +0900

    `UserProfileUseCase` can load current user's info and specified error

--- Engine/InstagramEngine/UseCase/UserProfileUseCase.swift
+++ Engine/InstagramEngine/UseCase/UserProfileUseCase.swift
@@ -7,3 +7,37 @@
 //
 
 import Foundation
+
+public protocol UserProfileClient {
+    func loadCurrentUserInfo(_ completion: @escaping (Result<UserEntity, UserProfileUseCase.Error>) -> Void)
+}
+
+public protocol UserProfileUseCaseOutput {
+    
+}
+
+final public class UserProfileUseCase {
+    
+    private let client: UserProfileClient
+    private let output: UserProfileUseCaseOutput
+    
+    init(client: UserProfileClient, output: UserProfileUseCaseOutput) {
+        self.client = client
+        self.output = output
+    }
+    
+    public enum Error: Swift.Error {
+        case currentUserNotExist
+    }
+    
+    public func loadProfile(_ completion: @escaping (Result<UserEntity, Error>) -> Void) {
+        client.loadCurrentUserInfo() { result in
+            switch result {
+            case .success(let user):
+                completion(.success(user))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
--- Engine/InstagramEngineTests/UserProfileUseCaseTests.swift
+++ Engine/InstagramEngineTests/UserProfileUseCaseTests.swift
@@ -7,27 +7,93 @@
 //
 
 import XCTest
+@testable import InstagramEngine
 
 class UserProfileUseCaseTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_init_doesNotRequestUserInfo() {
+        let (_, client) = makeSUT()
+        
+        XCTAssertEqual(client.requestedCount, 0)
     }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func test_loadProfileTwice_loadUserInfoTwice() {
+        let (sut, client) = makeSUT()
+        
+        var capturedUser = [UserEntity]()
+        sut.loadProfile { result in
+            if case let Result.success(user) = result {
+                capturedUser.append(user)
+            }
+        }
+        sut.loadProfile { result in
+            if case let Result.success(user) = result {
+                capturedUser.append(user)
+            }
+        }
+        
+        let user1 = UserEntity(id: "0",
+                               email: "dummy1@naver.com",
+                               username: "dummy1",
+                               profileImageUrl: "http://a-url.com")
+        let user2 = UserEntity(id: "1",
+                               email: "dummy2@naver.com",
+                               username: "dummy2",
+                               profileImageUrl: "http://b-url.com")
+        client.completeWithSuccess(user1, at: 0)
+        client.completeWithSuccess(user2, at: 1)
+        
+        XCTAssertEqual(capturedUser, [user1, user2])
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func test_loadProfile_deliverscurrentUserNotExistError() {
+        let client = UserProfileClientStub()
+        let output = UserProfileUseCaseOutputDummy()
+        let sut = UserProfileUseCase(client: client, output: output)
+        
+        var capturedError = [UserProfileUseCase.Error]()
+        sut.loadProfile { result in
+            if case let Result.failure(error) = result {
+                capturedError.append(error)
+            }
+        }
+        
+        let clientError = NSError(domain: "test", code: 0)
+        client.completeWithFailure(clientError)
+        
+        XCTAssertEqual(capturedError, [.currentUserNotExist])
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    
+    // MARK: - Helpers
+    
+    private func makeSUT() -> (sut: UserProfileUseCase, client: UserProfileClientStub) {
+        let client = UserProfileClientStub()
+        let output = UserProfileUseCaseOutputDummy()
+        let sut = UserProfileUseCase(client: client, output: output)
+        return (sut, client)
+    }
+    
+    private class UserProfileClientStub: UserProfileClient {
+        private var messages = [(Result<UserEntity, UserProfileUseCase.Error>) -> Void]()
+        
+        var requestedCount = 0
+        
+        func loadCurrentUserInfo(_ completion: @escaping (Result<UserEntity, UserProfileUseCase.Error>) -> Void) {
+            requestedCount += 1
+            messages.append(completion)
+        }
+        
+        func completeWithSuccess(_ user: UserEntity, at index: Int = 0) {
+            messages[index](.success(user))
+        }
+        
+        func completeWithFailure(_ error: Error, at index: Int = 0) {
+            messages[index](.failure(.currentUserNotExist))
         }
     }
-
+    
+    private class UserProfileUseCaseOutputDummy: UserProfileUseCaseOutput {
+        
+    }
 }
