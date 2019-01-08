42e6375120970a9fba593062a6d37e8d610074bf
undervine <undervine@gmail.com>
2019-01-08 22:14:32 +0900

    RegisterUserPresenter can deliver error message to the related view

---
+++ App/InstagramClientApp/Presenter/RegisterUserPresenter.swift
@@ -0,0 +1,31 @@
+//
+//  RegisterUserPresenter.swift
+//  InstagramClientApp
+//
+//  Created by 심승민 on 08/01/2019.
+//  Copyright © 2019 심승민. All rights reserved.
+//
+
+import InstagramEngine
+
+class RegisterUserPresenter: RegisterUserUseCaseOutput {
+    
+    private let view: RegisterUserView
+    
+    init(view: RegisterUserView) {
+        self.view = view
+    }
+    
+    func registerSucceeded() {
+        // move to MainTabBarViewController
+    }
+    
+    func registerFailed(_ error: RegisterUserUseCase.Error) {
+        view.display(error.localizedDescription)
+    }
+    
+}
+
+protocol RegisterUserView {
+    func display(_ errorMessage: String)
+}
---
+++ App/InstagramClientAppTests/Presenter/RegisterUserPresenterTests.swift
@@ -0,0 +1,52 @@
+//
+//  RegisterUserPresenterTests.swift
+//  InstagramClientAppTests
+//
+//  Created by 심승민 on 08/01/2019.
+//  Copyright © 2019 심승민. All rights reserved.
+//
+
+import XCTest
+import InstagramEngine
+@testable import InstagramClientApp
+
+class RegisterUserPresenterTests: XCTestCase {
+
+    func test_deliversTheRightErrorMessage_whenRegisterFailed() {
+        let view = RegisterUserViewStub()
+        let presenter = RegisterUserPresenter(view: view)
+        
+        let errors = [
+            RegisterUserUseCase.Error.accountExistsWithDifferentCredential,
+            .credentialAlreadyInUse,
+            .databaseUpdateError,
+            .emailAlreadyInUse,
+            .invalidEmail,
+            .invalidName,
+            .invalidPassword,
+            .networkError,
+            .storageUploadError,
+            .unknown,
+            .userDisabled,
+            .userNotFound,
+            .wrongPassword
+        ]
+        
+        errors.forEach {
+            presenter.registerFailed($0)
+        }
+        
+        XCTAssertEqual(view.stubbedErrorMessages, errors.map { $0.localizedDescription })
+    }
+
+    
+    // MARK: - Helpers
+    
+    private class RegisterUserViewStub: RegisterUserView {
+        var stubbedErrorMessages = [String]()
+        
+        func display(_ errorMessage: String) {
+            stubbedErrorMessages.append(errorMessage)
+        }
+    }
+}
