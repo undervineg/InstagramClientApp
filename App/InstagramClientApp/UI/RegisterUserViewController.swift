bfcc324c220cba35aa76ec94bd24f43bb87fa6cf
undervine <undervine@gmail.com>
2019-01-13 21:11:09 +0900

    Break down codes into private functions

--- App/InstagramClientApp/UI/RegisterUserViewController.swift
+++ App/InstagramClientApp/UI/RegisterUserViewController.swift
@@ -20,7 +20,7 @@ final class RegisterUserViewController: UIViewController {
     
     private var router: RegisterRouter.Routes?
     
-    var registerCallback: ((String, String, String, Data) -> ())?
+    var register: ((String, String, String, Data) -> ())?
     
     convenience init(router: RegisterRouter.Routes) {
         self.init()
@@ -29,34 +29,37 @@ final class RegisterUserViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        profileImageButton.layer.cornerRadius = profileImageButton.frame.width/2
-        profileImageButton.layer.masksToBounds = true
+        configureUIExtras()
+    }
+    
+    @IBAction func openImagePicker(_ sender: UIButton) {
+        openEditableImagePicker()
+    }
+    
+    @IBAction func handleTextInputChange(_ sender: UITextField) {
+        makeButtonEnableAndColored()
+    }
+    
+    @IBAction func signUp(_ sender: UIButton) {
+        validateTextFieldsAndRegisterUser()
+    }
+    
+    @IBAction func routeToLoginPage(_ sender: UIButton) {
+        router?.openLoginPage()
+    }
+    
+    
+    // MARK: - Private Methods
+    
+    private func isAllTextFieldsValid() -> Bool {
+        let isEmailValid = emailTextField.text?.count ?? 0 > 0
+        let isUsernameValid = usernameTextField.text?.count ?? 0 > 0
+        let isPasswordValid = passwordTextField.text?.count ?? 0 > 0
         
-        signUpButton.isEnabled = false
-        signUpButton.backgroundColor = UIColor(red: 123/255, green: 115/255, blue: 231/255, alpha: 0.5)
-        
-        profileImageButton.addTarget(self, action: #selector(openImagePicker(_:)), for: .touchUpInside)
-        signUpButton.addTarget(self, action: #selector(signUp(_:)), for: .touchUpInside)
-        emailTextField.addTarget(self, action: #selector(handleTextInputChange), for: .editingChanged)
-        usernameTextField.addTarget(self, action: #selector(handleTextInputChange), for: .editingChanged)
-        passwordTextField.addTarget(self, action: #selector(handleTextInputChange), for: .editingChanged)
+        return isEmailValid && isUsernameValid && isPasswordValid
     }
     
-    @objc private func openImagePicker(_ sender: UIButton) {
-        let imagePicker = UIImagePickerController()
-        imagePicker.delegate = self
-        imagePicker.allowsEditing = true
-
-        router?.openImagePicker(imagePicker, with: nil)
-    }
-    
-    @objc private func handleTextInputChange(_ sender: UITextField) {
-        signUpButton.backgroundColor = isAllTextFieldsValid() ? UIColor(red: 123/255, green: 115/255, blue: 231/255, alpha: 1) : UIColor(red: 123/255, green: 115/255, blue: 231/255, alpha: 0.5)
-        signUpButton.isEnabled = isAllTextFieldsValid() ? true : false
-    }
-    
-    @objc private func signUp(_ sender: UIButton) {
+    private func validateTextFieldsAndRegisterUser() {
         guard isAllTextFieldsValid() else { return }
         
         let email = emailTextField.text ?? ""
@@ -65,15 +68,28 @@ final class RegisterUserViewController: UIViewController {
         let profileImageData = profileImageButton.imageView?.image?.jpegData(compressionQuality: 0.3)
         
         indicatorView.startAnimating()
-        registerCallback?(email, username, password, profileImageData!)
+        register?(email, username, password, profileImageData!)
     }
     
-    private func isAllTextFieldsValid() -> Bool {
-        let isEmailValid = emailTextField.text?.count ?? 0 > 0
-        let isUsernameValid = usernameTextField.text?.count ?? 0 > 0
-        let isPasswordValid = passwordTextField.text?.count ?? 0 > 0
+    private func openEditableImagePicker() {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
         
-        return (isEmailValid && isUsernameValid && isPasswordValid)
+        router?.openImagePicker(imagePicker, with: nil)
+    }
+    
+    private func makeButtonEnableAndColored() {
+        signUpButton.backgroundColor = isAllTextFieldsValid() ? UIColor(red: 123/255, green: 115/255, blue: 231/255, alpha: 1) : UIColor(red: 123/255, green: 115/255, blue: 231/255, alpha: 0.5)
+        signUpButton.isEnabled = isAllTextFieldsValid()
+    }
+    
+    private func configureUIExtras() {
+        profileImageButton.layer.cornerRadius = profileImageButton.frame.width/2
+        profileImageButton.layer.masksToBounds = true
+        
+        signUpButton.isEnabled = false
+        signUpButton.backgroundColor = UIColor(red: 123/255, green: 115/255, blue: 231/255, alpha: 0.5)
     }
 }
 
@@ -100,6 +116,8 @@ extension RegisterUserViewController: RegisterUserView {
     func display(_ errorMessage: String) {
         let alert = UIAlertController(title: nil, message: errorMessage, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "확인", style: .default, handler: nil))
-        present(alert, animated: true, completion: nil)
+        present(alert, animated: true) {
+            self.indicatorView.stopAnimating()
+        }
     }
 }
