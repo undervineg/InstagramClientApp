072efe3427ae29314ef9acff58e694bc0a74d557
undervine <undervine@gmail.com>
2019-01-15 15:32:00 +0900

    Block `plus` tab bar button not to be selected

--- App/InstagramClientApp/UI/MainTabBarViewController.swift
+++ App/InstagramClientApp/UI/MainTabBarViewController.swift
@@ -8,8 +8,8 @@
 
 import UIKit
 
-final class MainTabBarViewController: UITabBarController {
-    
+final class MainTabBarViewController: UITabBarController, UITabBarControllerDelegate {
+
     convenience init(subViewControllers: [UIViewController]) {
         self.init()
         viewControllers = subViewControllers
@@ -18,7 +18,15 @@ final class MainTabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.delegate = self
+        
         tabBar.tintColor = .black
     }
 
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        if viewControllers?.firstIndex(of: viewController) == 2 {
+            return false
+        }
+        return true
+    }
 }
